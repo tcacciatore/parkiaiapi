f4992ec0fb5c4e8da20eaee4882dd2675e973d19
Thomas Cacciatore <cacciato.thomas@gmail.com>
2025-04-18 14:04:27 +0200

    add cors

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_cors import CORS
 from routes import dishes, medication, exercises, songs, metrics
 
 def create_app():
     app = Flask(__name__)
+    
+    # Activer CORS pour toutes les routes
+    CORS(app)
+
+    # Si vous voulez restreindre les origines autorisées :
+    # CORS(app, resources={r"/*": {"origins": "http://localhost:53801"}})
 
     # Enregistrement des blueprints
     app.register_blueprint(dishes.bp)
